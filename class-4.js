3cf1298c784666ee85e955e3e479ae9270ec14c2
dayannt <nicdayant10@gmail.com>
2025-02-28 18:31:11 -0500

    Update class-4.js

--- class-4.js
+++ class-4.js
@@ -1,4 +1,3 @@
-//Planteamiento:cree una función de JS que analice un arreglo de números que representa notas de estudiantesy retorne un objeto con información basado en el siguiente mapa:90-100: A80-89: B70-79: C60-69: DBelow 60: FEjemplo: const studentGrades = [100, 50, 65, 70, 85, 96, 45];const result = computeGrades(studentGrades)console.log(result)console:{parsed: [A, F, D, C, B, A, F],passed: 5,lose: 2}
 
 const studentGrades = [100, 50, 65, 70, 85, 96, 45];
 const computeGrades = (studentGrades) => {
